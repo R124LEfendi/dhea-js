e2952d49c73b7d23bc7febfc423273f65d05d9f9
R124LEfendi <rizalgaras@gmail.com>
2023-07-11 14:40:09 +0700

    update content

---
+++ percobaan.js
@@ -0,0 +1,15 @@
+let nama = "Rizal Efendi";
+let usia = 27;
+let tinggiBadan = 165.5;
+let beratBadan;
+let calonIstri = 1;
+
+beratBadan = 55;
+
+if (calonIstri == null) {
+  calonIstri = "belum punya";
+} else calonIstri = "Tika Tri Ayunda";
+
+document.writeln(`nama saya adalah ${nama} usia saya adalah ${usia}
+tinggi badan saya adalah ${tinggiBadan}cm berat badan saya adalah ${beratBadan}kg
+calon istri saya adalah ${calonIstri}`);
