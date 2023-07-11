4a707e3243ea20bc4157bf3b52e15e264606d7ee
R124LEfendi <rizalgaras@gmail.com>
2023-07-11 14:25:29 +0700

    update content

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-// alert("hello world");
+let nama = "rizal efendi";
+let usia = 27;
+let tinggiBadan = 165.5;
+let beratBadan;
+let pacar = 1;
 
-let usia = prompt("berapa usia anda?");
-alert("usia anda adalah " + usia);
+beratBadan = 55;
+
+if (pacar == null) {
+  pacar = "belum punya";
+} else {
+  pacar = "Tika Tri Ayunda";
+}
+
+alert(`nama saya adalah ${nama} usia saya adalah ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya adalah ${beratBadan}kg
+dan pacar saya ${pacar} `);
